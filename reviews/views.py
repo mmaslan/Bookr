23a207b407a9686d24bd9d26247f5d998cf34240
mmaslan <marek.maslany1@gmail.com>
2022-09-24 19:29:29 +0200

    1.2

--- reviews/views.py
+++ reviews/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "base.html")
+
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
