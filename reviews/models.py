754de875aca8d204f235a86537daaaaf4ec163bc
marek <marek.maslany1@gmail.com>
2022-10-03 12:45:13 +0200

    Models

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib import auth
 
 
 class Publisher(models.Model):
     name = models.CharField(max_length=150, help_text='Nazwa wydawnictwa.')
     website = models.URLField(help_text='Witryna wydawnictwa.')
     email = models.EmailField(help_text='Adres email wydawnictwa.')
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -14,11 +18,17 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     contributors = models.ManyToManyField('Contributor', through='BookContributor')
 
+    def __str__(self):
+        return self.title
+
 
 class Contributor(models.Model):
     first_name = models.CharField(max_length=50, help_text='Imię lub imiona współtwórców.')
     last_name = models.CharField(max_length=50, help_text='Nazwisko lub nazwiska współtwórców')
     email = models.EmailField(help_text='Email współtwórcy.')
+
+    def __str__(self):
+        return self.first_name
 
 
 class BookContributor(models.Model):
@@ -31,3 +41,12 @@ class BookContributor(models.Model):
     contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
     role = models.CharField(verbose_name='Rola jaką współtwórca odegrał podczas tworzenia tej książki.',
                             choices=ContributionRole.choices, max_length=20)
+
+
+class Review(models.Model):
+    content = models.TextField(help_text='Teksts recenzji.')
+    rating = models.IntegerField(help_text='Ocena użytkownika')
+    date_created = models.DateTimeField(auto_now_add=True, help_text='Data i czas utworzenia recenzji.')
+    date_edited = models.DateTimeField(null=True, help_text='Data i czas ostatniej edycji recenzji.')
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, help_text='Recenzowana książka.')
