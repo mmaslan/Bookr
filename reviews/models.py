f21761129e9a63011b1cb3d95908ed8ec3c9dddb
marek <marek.maslany1@gmail.com>
2022-10-03 12:03:12 +0200

    Models

--- reviews/models.py
+++ reviews/models.py
@@ -5,3 +5,15 @@ class Publisher(models.Model):
     name = models.CharField(max_length=150, help_text='Nazwa wydawnictwa.')
     website = models.URLField(help_text='Witryna wydawnictwa.')
     email = models.EmailField(help_text='Adres email wydawnictwa.')
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=70, help_text='Tutuł książki.')
+    publication_date = models.DateField(verbose_name='Data publikacji książki.')
+    isbn = models.CharField(max_length=20, verbose_name='Numer ISBN książki.')
+
+
+class Contributor(models.Model):
+    first_name = models.CharField(max_length=50, help_text='Imię lub imiona współtwórców.')
+    last_name = models.CharField(max_length=50, help_text='Nazwisko lub nazwiska współtwórców')
+    email = models.EmailField(help_text='Email współtwórcy.')
