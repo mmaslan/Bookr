5bd37af7f91ec066e66b4863cb3cf26965a95278
Marek Maślany <marekmaslany@Mareks-MacBook-Air.local>
2022-10-03 11:18:51 +0200

    Reviews/models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=150, help_text='Nazwa wydawnictwa.')
+    website = models.URLField(help_text='Witryna wydawnictwa.')
+    email = models.EmailField(help_text='Adres email wydawnictwa.')
