657207adfb3fa456eafa40a49431dd78bb4b56ea
mmaslan <marek.maslany1@gmail.com>
2022-10-09 21:07:17 +0200

    Urls and function views

--- bookr/urls.py
+++ bookr/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import reviews.views
+from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book-search', reviews.views.book_search)
-]
+
+urlpatterns = [path('admin/'. admin.site.urls),\
+               path('', include('reviews.urls'))]
