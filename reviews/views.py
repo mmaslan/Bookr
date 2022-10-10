9173109c47d61b2b7ab486a3fb14983429a18980
mmaslan <marek.maslany1@gmail.com>
2022-10-10 19:23:40 +0200

    Templates

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
-from models import Book
-
+from django.shortcuts import render
 
 def welcome_view(request):
-    message = f"<html><h1>Witaj w witrynie Bookr!</h1>"\
-        "<p>{Book.objects.count()} książek i stale dodajemy nowe!</p></html>"
-    return HttpResponse(message)
+    return render(request, 'base.html')
