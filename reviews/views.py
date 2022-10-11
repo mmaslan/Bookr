3010a72ce5ec05d83edad63e4d1890ff06cfbfbf
mmaslan <marek.maslany1@gmail.com>
2022-10-11 20:08:30 +0200

    3.3

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .models import Book, Review
+from .utils import average_rating
 
-def welcome_view(request):
-    return render(request, 'base.html')
+
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+    else:
+        book_rating = 0
+        number_of_reviews = 0
+        book_list.append({'book': book, 'book_rating': book_rating, 'number_of_reviews': number_of_reviews})
+        context = {'book_list': book_list}
+    return render(request, 'reviews/book_list.html', context)
