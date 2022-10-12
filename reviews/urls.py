62c4918ed1618977794b2329ea903fb0314e7109
mmaslan <marek.maslany1@gmail.com>
2022-10-12 21:57:13 +0200

    Book_list

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('', views.welcome_view, name='welcome_view'),]
+urlpatterns = [path('', views.book_list, name='book_list'),]
