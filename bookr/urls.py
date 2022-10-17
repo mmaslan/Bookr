6e1c75448e12deac087a8458da14aa235b6d1114
mmaslan <marek.maslany1@gmail.com>
2022-10-17 21:36:46 +0200

    Update

--- bookr/urls.py
+++ bookr/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-urlpatterns = [path('admin/', admin.site.urls),
-               path('', include('reviews.urls'))]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('reviews.urls'))
+]
