736de70be300fd8be74f94fbccde72c730a63aaa
mmaslan <marek.maslany1@gmail.com>
2022-10-17 21:12:23 +0200

    Update

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
-
-urlpatterns = [path('books/', views.book_list, name='book_list'),]
+urlpatterns = [
+    path('books/', views.book_list, name='book_list')
+]
