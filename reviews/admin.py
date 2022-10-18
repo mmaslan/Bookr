6c6658c3c72bdea7bbc90f2677010bbf39e08f6b
mmaslan <marek.maslany1@gmail.com>
2022-10-18 14:17:20 +0200

    Admin Reviews models

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import (
+    Publisher,
+    Contributor,
+    Book,
+    BookContributor,
+    Review
+)
+
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(BookContributor)
+admin.site.register(Review)
