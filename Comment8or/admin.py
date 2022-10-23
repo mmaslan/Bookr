10ab0baf81f88b60c8947869309f3dac98c98aa9
mmaslan <marek.maslany1@gmail.com>
2022-10-23 20:49:42 +0200

    Update 4.1

--- Comment8or/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-
-class Comment8orAdminSite(admin.AdminSite):
-    title_header = 'c8admin'
-    site_header = 'c8 admin site'
-    index_title = 'c8admin'
-    logout_template =
--- Comment8or/messageboard/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessageboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messageboard'
