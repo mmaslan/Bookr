f10ac0c28a6fdd3ddbf33e3186f669e5fda3882a
mmaslan <marek.maslany1@gmail.com>
2022-11-02 19:15:23 +0100

    Update - list_filter / BookAdmin.reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -9,8 +9,23 @@ from reviews.models import (
 )
 
 
+def isbn13(obj):
+    return "{}-{}-{}-{}-{}".format(obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6],
+                                   obj.isbn[6:12], obj.isbn[12:13])
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'isbn')
+    list_filter = ('publisher',)
+
+
+def initialled_name(obj):
+    initials = ''.join([name[0] for name in obj.first_name.split(' ')])
+    return "{}, {}".format(obj.last_names, initials)
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (initialled_name,)
 
 
 admin.site.register(Publisher)
