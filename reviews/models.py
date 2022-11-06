649a79369e25d7b9ca5d649bad5123d8337cb545
mmaslan <marek.maslany1@gmail.com>
2022-11-06 20:57:36 +0100

    Update

--- reviews/models.py
+++ reviews/models.py
@@ -3,9 +3,11 @@ from django.contrib import auth
 
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=150, help_text='Nazwa wydawnictwa.')
-    website = models.URLField(help_text='Witryna wydawnictwa.')
-    email = models.EmailField(help_text='Adres email wydawnictwa.')
+    """A company that publishes books."""
+    name = models.CharField(max_length=50,
+                            help_text="The name of the Publisher.")
+    website = models.URLField(help_text="The Publisher's website.")
+    email = models.EmailField(help_text="The Publisher's email address.")
 
     def __str__(self):
         return self.name
@@ -35,12 +37,18 @@ class Book(models.Model):
 
 
 class Contributor(models.Model):
-    first_names = models.CharField(max_length=50, help_text='Imię lub imiona współtwórców.')
-    last_names = models.CharField(max_length=50, help_text='Nazwisko lub nazwiska współtwórców')
-    email = models.EmailField(help_text='Email współtwórcy.')
+    """A contributor to a Book, e.g. author, editor, co-author."""
+    first_names = models.CharField(max_length=50,
+                                   help_text="The contributor's first name or names.")
+    last_names = models.CharField(max_length=50,
+                                  help_text="The contributor's last name or names.")
+    email = models.EmailField(help_text="The contact email for the contributor.")
 
     def initialled_name(self):
-        initials = ''.join([name[0] for name in self.first_names.split(' ')])
+        """ self.first_names='Jerome David', self.last_names='Salinger'
+            => 'Salinger, JD' """
+        initials = ''.join([name[0] for name
+                            in self.first_names.split(' ')])
         return "{}, {}".format(self.last_names, initials)
 
     def __str__(self):
@@ -49,22 +57,26 @@ class Contributor(models.Model):
 
 class BookContributor(models.Model):
     class ContributionRole(models.TextChoices):
-        AUTHOR = 'AUTHOR', 'Author'
-        CO_AUTHOR = 'CO_AUTHOR', 'Co-Author'
-        EDITOR = 'EDITOR', 'Editor'
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
 
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
-    role = models.CharField(verbose_name='Rola jaką współtwórca odegrał podczas tworzenia tej książki.',
+    role = models.CharField(verbose_name="The role this contributor had in the book.",
                             choices=ContributionRole.choices, max_length=20)
 
 
 class Review(models.Model):
-    content = models.TextField(help_text='Teksts recenzji.')
-    rating = models.IntegerField(help_text='Ocena użytkownika')
-    date_created = models.DateTimeField(auto_now_add=True, help_text='Data i czas utworzenia recenzji.')
-    date_edited = models.DateTimeField(null=True, help_text='Data i czas ostatniej edycji recenzji.')
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The the reviewer has given.")
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(null=True,
+                                       help_text='''The date and time the review was last edited.'''
+                                       )
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, help_text='Recenzowana książka.')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             help_text="The Book that this review is for.")
 
 
