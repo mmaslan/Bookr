fabfef70897829124e591a9bf0c5edea1dd2c4c4
mmaslan <marek.maslany1@gmail.com>
2022-11-10 22:30:28 +0100

    5.4

--- business_site/business_site/settings.py
+++ business_site/business_site/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_production_test')
