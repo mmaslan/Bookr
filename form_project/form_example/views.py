8d8d5abc839c07bb66e61d7da22a7d4e845251c2
mmaslan <marek.maslany1@gmail.com>
2022-11-16 20:55:30 +0100

    Update

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def form_example(request):
+    return render(request, "form-example.html")
--- form_project/form_project/urls.py
+++ form_project/form_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import form_example.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('form-example/', form_example.views.form_example)
 ]
