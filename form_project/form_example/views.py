326845423c559d58267b8a21b9da4ffcc7bd3e74
mmaslan <marek.maslany1@gmail.com>
2022-11-17 18:15:52 +0100

    6.2

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def form_example(request):
-    return render(request, "form-example.html")
+    for name in request.POST:
+        print('{}: {}'.format(name, request.POST.getlist(name)))
+    return render(request, "form-example.html", {'method': request.method})
