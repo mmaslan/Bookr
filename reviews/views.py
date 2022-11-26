4e6e73e4bade4edf215ced1fc4335402d4a2ed61
mmaslan <marek.maslany1@gmail.com>
2022-11-26 16:38:33 +0100

    6.1

--- reviews/views.py
+++ reviews/views.py
@@ -13,6 +13,16 @@ def book_search(request):
     search_text = request.GET.get("search", "")
     form = SearchForm(request.GET)
     books = set()
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in") or "title"
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        else:
+
+
 
     return render(request, "reviews/search-results.html", {'form': form, 'search_text': search_text, 'books': books})
 
