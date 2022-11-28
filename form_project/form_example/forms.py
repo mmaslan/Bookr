98d0d82671bd24f79d59721b95cf647b86c3ad4b
mmaslan <marek.maslany1@gmail.com>
2022-11-28 21:07:35 +0100

    7.1

--- form_project/form_example/forms.py
+++ form_project/form_example/forms.py
@@ -1,38 +1,27 @@
 from django import forms
-
-RADIO_CHOICES = (
-    ("Wartość pierwsza", "Wyświetlona pierwsza wartość"),
-    ("Wartośćc druga", "Wyświetlona druuga wartość"),
-    ("Wartość trzecia", "Wyświetlona trzecia wartość")
-)
-
-BOOK_CHOICES = (
-    (
-        "Literatura faktu", (
-            ("1", "Deep Learning with Keras"),
-            ("2", "Web Development with Django")
-        )
-    ),
-    (
-        "Literatura Piękna", (
-            ("3", "Brave New World"),
-            ("4", "The Great Gatsby")
-        )
-    )
-)
+from django.core.exceptions import ValidationError
 
 
-class ExampleForm(forms.Form):
-    text_input = forms.CharField(max_length=3)
-    password_input = forms.CharField(min_length=8, widget=forms.PasswordInput)
-    checkbox_on = forms.BooleanField()
-    radio_input = forms.ChoiceField(choices=RADIO_CHOICES, widget=forms.RadioSelect)
-    favorite_book = forms.ChoiceField(choices=BOOK_CHOICES)
-    books_you_own = forms.MultipleChoiceField(required=False, choices=BOOK_CHOICES)
-    text_area = forms.CharField(widget=forms.Textarea)
-    integer_input = forms.IntegerField(min_value=1, max_value=10)
-    float_input = forms.FloatField()
-    decimal_input = forms.DecimalField(max_digits=3)
-    email_input = forms.EmailField()
-    date_input = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
-    hidden_input = forms.CharField(widget=forms.HiddenInput, initial="Ukryta wartość")
+def validate_email_domain(value):
+    if value.split("@")[-1].lower() != "example.com":\
+        raise ValidationError("Adres e-mail musi należeć do domeny example.com")
+
+
+class OrderForm(forms.Form):
+    magazine_count = forms.IntegerField(min_value=0, max_value=80)
+    book_count = forms.IntegerField(min_value=0, max_value=50)
+    send_confirmation = forms.BooleanField(required=False)
+    email = forms.EmailField(required=False, validators=[validate_email_domain])
+
+    def clean_email(self):
+        return self.cleaned_data['email'].lower()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['send_confirmation'] and not cleaned_data.get('email'):
+            self.add_error('email', 'Wpisz adres e-mail, aby otrzymać potwierdzenie.')
+        elif cleaned_data.get('email') and not cleaned_data['send_confirmation']:
+            self.add_error('send_confirmation', 'Zaznacz to pole, jeżeli chcesz otrzymać email z potwierdzeniem.')
+        item_total = cleaned_data.get('magazine_count', 0) + cleaned_data.get('book_count', 0)
+        if item_total > 100:
+            self.add_error(None, 'Całkowita liczba produktów nie może przekroczyć 100.')
