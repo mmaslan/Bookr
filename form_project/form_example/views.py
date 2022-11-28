b0ad51b2f95f71b44e0d8c597c8d7243c5257bef
mmaslan <marek.maslany1@gmail.com>
2022-11-28 21:20:42 +0100

    7.1

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from .forms import ExampleForm
+from .forms import OrderForm
 
 
 def form_example(request):
-    if request.method == 'POST':
-        form = ExampleForm(request.POST)
+    if request.method == "POST":
+        form = OrderForm(request.POST)
     else:
-        form = ExampleForm()
+        form = OrderForm()
 
-    if request.method == 'POST':
-        form = ExampleForm(request.POST)
+    if request.method == "POST":
+        form = OrderForm(request.POST)
         if form.is_valid():
             for name, value in form.cleaned_data.items():
                 print("{}: ({}) {}".format(name, type(value), value))
