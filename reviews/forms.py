157a0b958ee85157c1fff4c43ea2564bdcefab59
mmaslan <marek.maslany1@gmail.com>
2022-11-29 22:16:36 +0100

    7.2 Activity

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from .models import Publisher, Review
 
 
 class SearchForm(forms.Form):
     search = forms.CharField(required=False, min_length=3)
     search_in = forms.ChoiceField(required=False, choices=(("title", "Title"), ("contributor", "Contributor")))
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ['date_edited', 'book']
+
+    rating = forms.IntegerField(min_value=0, max_value=5)
