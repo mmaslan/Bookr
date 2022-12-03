4f11890557032b79eb3d3ae6606e1c67e7760533
mmaslan <marek.maslany1@gmail.com>
2022-12-03 17:04:09 +0100

    8.2

--- media_project/media_project/urls.py
+++ media_project/media_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import media_example.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('media-example/', media_example.views.media_example)
 ]
 
 if settings.DEBUG:
