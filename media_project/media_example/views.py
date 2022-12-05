6bb892297ccb43f3fcabb79c90fbd908733f38d9
mmaslan <marek.maslany1@gmail.com>
2022-12-05 22:54:06 +0100

    8.3

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 
 def media_example(request):
+    if request.method == 'POST':
+        save_path = os.path.join(settings.MEDIA_ROOT, request.FILES['file_upload'].name)
+        with open(save_path, 'wb') as output_file:
+            for chunk in reuqest.FILES['file_upload'].chunks():
+                output_file.write(chunk)
     return render(request, 'media-example.html')
