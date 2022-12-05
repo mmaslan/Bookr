3b436b6fe5e744ee5e6aaebb6ca1cd6e2842700f
mmaslan <marek.maslany1@gmail.com>
2022-12-05 22:55:26 +0100

    8.3

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -7,6 +7,6 @@ def media_example(request):
     if request.method == 'POST':
         save_path = os.path.join(settings.MEDIA_ROOT, request.FILES['file_upload'].name)
         with open(save_path, 'wb') as output_file:
-            for chunk in reuqest.FILES['file_upload'].chunks():
+            for chunk in request.FILES['file_upload'].chunks():
                 output_file.write(chunk)
     return render(request, 'media-example.html')
