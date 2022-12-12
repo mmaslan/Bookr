9bfa9596728dd6fa26c8f4ef5ac177166abd91fe
mmaslan <marek.maslany1@gmail.com>
2022-12-12 20:47:34 +0100

    8.4

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
 import os
-
+from .forms import UploadForm
 
 def media_example(request):
     if request.method == 'POST':
-        save_path = os.path.join(settings.MEDIA_ROOT, request.FILES['file_upload'].name)
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            save_path = os.path.join(settings.MEDIA_ROOT, request.FILES['file_upload'].name)
         with open(save_path, 'wb') as output_file:
-            for chunk in request.FILES['file_upload'].chunks():
+            for chunk in form.cleaned_data["file_upload"].chunks():
                 output_file.write(chunk)
-    return render(request, 'media-example.html')
+
+    else:
+        form = UploadForm()
+
+    return render(request, 'media-example.html', {'form': form})
