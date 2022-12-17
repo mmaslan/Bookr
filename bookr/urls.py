ea857178dd66374c66f4f4624d6ac581e9ce3019
mmaslan <marek.maslany1@gmail.com>
2022-12-17 19:34:07 +0100

    9.2

--- bookr/urls.py
+++ bookr/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from reviews.admin import admin
 from django.urls import include, path
-import reviews.views
 from django.conf import settings
 from django.conf.urls.static import static
+from bookr.views import profile
 
 
 urlpatterns = [
     path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
+    path('accounts/profile', profile, name='profile'),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ]
