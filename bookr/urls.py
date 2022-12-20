2d5d4af604f46955a8e670f98ae99be37dc05fbf
mmaslan <marek.maslany1@gmail.com>
2022-12-20 14:59:55 +0100

    Update

--- bookr/urls.py
+++ bookr/urls.py
@@ -17,12 +17,9 @@ from reviews.admin import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from bookr.views import profile
 
 
 urlpatterns = [
-    path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
-    path('accounts/profile', profile, name='profile'),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
     path('books/<int:book_pk>/reviews/new/', views.review_edit, name='review_create'),
     path('books/<int:book_pk>/reviews/<int:review_pk>/', views.review_edit, name='review_edit'),
+    path('books/<int:pk>/media/', views.book_media, name='book_media'),
     path('book-search/', views.book_search, name='book_search'),
-    path('publishers/<int:pk>/', views.publisher_edit, name='publisher_edit'),
-    path('publishers/new/', views.publisher_edit, name='publisher_create'),
+    path('publishers/<int:pk>/',views.publisher_edit, name='publisher_edit'),
+    path('publishers/new/',views.publisher_edit, name='publisher_create'),
 ]
