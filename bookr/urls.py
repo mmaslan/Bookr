59f521f223269bdb35702ec0230910d1cb2739ee
mmaslan <marek.maslany1@gmail.com>
2022-12-29 21:42:29 +0100

    10.1

--- bookr/urls.py
+++ bookr/urls.py
@@ -17,11 +17,13 @@ from reviews.admin import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from bookr_admin.admin import admin_site
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
+    path('admin/', admin_site.urls)
 ]
 
 if settings.DEBUG:
